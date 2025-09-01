550afd83e31c8fceb3b35355053f1edc7c61dc6f
fa230299 <afifi_fahrul@hotmail.com>
2025-09-01 11:26:15 +0700

    tambahkan

---
+++ latihan-js-dasar/komentar.js
@@ -0,0 +1,7 @@
+//Komentar satu baris
+
+/*Komentar beberapa baris
+menjelaskan  bagian program
+*/
+let nama = "Lavender"; // menyimpan nama siswa
+console.log("Nama saya:", nama); //Tampilkan ke layar
